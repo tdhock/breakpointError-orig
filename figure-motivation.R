b59db8a698ac043930c68cc684cdf19d4eb0e3db
Toby Dylan Hocking <tdhock5@gmail.com>
2014-05-29 13:23:46 -0400

    fig 1,2,3 captions

--- figure-motivation.R
+++ figure-motivation.R
@@ -6,6 +6,8 @@ works_with_R("3.1.0",
              "tdhock/ggplot2@98cefe4d653ce8f214177b66dc030c2f3c725ffb",
              changepoint="1.1.1")
 
+set.seed(5)
+
 params <-
   list(mean=data.frame(mean=c(-1, 1/2, -1/2),
          sd=c(1, 1, 1)),
@@ -13,6 +15,7 @@ params <-
          sd=c(1, 3, 1/2)))
 segment.last <- c(300, 400, 500)
 LAST.BASE <- as.integer(max(segment.last))
+is.sampled <- 1:LAST.BASE %in% sample(LAST.BASE, 100) # sampled points.
 change.after <- segment.last[-length(segment.last)]
 segment.first <- c(1, change.after+1)
 segments <- NULL
@@ -21,7 +24,6 @@ changes <- data.frame(change.after, what="truth")
 model.segments <- NULL
 err.df <- NULL
 model.changes <- NULL
-set.seed(1)
 for(change.param in names(params)){
   param.df <- params[[change.param]]
   facet <- if(change.param=="var"){
@@ -36,7 +38,7 @@ for(change.param in names(params)){
   segments <- rbind(segments, these.segments)
   mean.vec <- NULL
   sd.vec <- NULL
-  sampled.signal <- NULL
+  full.signal <- NULL
   for(segment.i in 1:nrow(these.segments)){
     seg <- these.segments[segment.i,]
     mean.vec <- c(mean.vec, rep(seg$mean, seg$size))
@@ -45,14 +47,10 @@ for(change.param in names(params)){
     signal <- rnorm(seg$size, seg$mean, seg$sd)
     full.segment <- data.frame(base, signal, segment.i, change.param,
                                what="data", facet)
-    is.sampled <- sample(c(TRUE, FALSE),
-                         size=nrow(full.segment),
-                         replace=TRUE,
-                         prob=c(1, 4))
-    sampled.segment <- full.segment[is.sampled, ]
-    signals <- rbind(signals, sampled.segment)
-    sampled.signal <- rbind(sampled.signal, sampled.segment)
+    full.signal <- rbind(full.signal, full.segment)
   }
+  sampled.signal <- full.signal[is.sampled, ]
+  signals <- rbind(signals, sampled.signal)
   max.segments <- 5
   for(model.type in c("mean", "var")){
     fun.name <- paste0("cpt.", model.type)
@@ -147,7 +145,11 @@ for(change.param in names(params)){
 }
 
 model.colors <- brewer.pal(3, "Dark2")
-names(model.colors) <- c("truth", "mean", "var")
+##names(model.colors) <- c("truth", "mean", "var")
+names(model.colors) <- c("truth", "estimate")
+mtype <- function(x){
+  factor(ifelse(x=="truth", "truth", "estimate"), c("truth", "estimate"))
+}
 
 p <- ggplot(err.df, aes(segments,error))+
   geom_line(aes(size=type,colour=type,linetype=type))+
@@ -159,7 +161,9 @@ p <- ggplot(err.df, aes(segments,error))+
   theme(panel.margin=grid::unit(0, "cm"))
 dl <- direct.label(p+guides(linetype="none",colour="none",size="none"),
                    dl.combine("first.polygons","last.polygons"))
+print(dl)
 
+base.breaks <- c(segment.first, LAST.BASE)
 sig.model.breaks <- 
 ggplot()+
   geom_point(aes(base, signal), data=signals, pch=1)+
@@ -167,7 +171,7 @@ ggplot()+
                color=model.type, linetype=model.type),
            data=model.changes, show_guide=TRUE)+
   scale_x_continuous("base position",
-                     breaks=segment.first)+
+                     breaks=base.breaks)+
   facet_grid(segments ~ change.param)+
   theme_bw()+
   theme(panel.margin=grid::unit(0, "cm"))
@@ -180,7 +184,7 @@ ggplot()+
                color=model.type, linetype=model.type),
            data=model.changes, show_guide=TRUE)+
   scale_x_continuous("base position",
-                     breaks=segment.first)+
+                     breaks=base.breaks)+
   facet_grid(segments ~ change.param)+
   theme_bw()+
   theme(panel.margin=grid::unit(0, "cm"))
@@ -216,7 +220,7 @@ ggplot()+
                color=model.type, linetype=model.type),
            data=matching$changes, show_guide=TRUE)+
   scale_x_continuous("base position",
-                     breaks=segment.first)+
+                     breaks=base.breaks)+
   facet_grid(segments ~ change.param)+
   theme_bw()+
   theme(panel.margin=grid::unit(0, "cm"))
@@ -226,30 +230,32 @@ alpha.rect <- 5/10
 segments$facet2 <- ffactor("truth")
 changes$facet2 <- ffactor("truth")
 mod.ord <- c("truth", "mean", "var")
+model.breaks <- c("truth", "estimate")
 modelOnly <- 
 ggplot()+
   geom_point(aes(base, signal), data=signals, pch=1)+
   geom_rect(aes(xmin=first.base-1/2, xmax=last.base+1/2,
                 ymin=seg.mean-seg.sd, ymax=seg.mean+seg.sd,
-                fill=model.type),
+                fill=mtype(model.type)),
             data=matching$segments, alpha=alpha.rect, color=NA)+
   geom_segment(aes(first.base-1/2, seg.mean, xend=last.base+1/2, yend=seg.mean,
-                   color=model.type),
+                   color=mtype(model.type)),
                data=matching$segments)+
   scale_x_continuous("base position",
-                     breaks=segment.first)+
+                     breaks=base.breaks)+
   facet_grid(facet2 ~ facet)+
   theme_bw()+
   theme(panel.margin=grid::unit(0, "cm"))+
   geom_rect(aes(xmin=first-1/2, xmax=last+1/2,
                 ymin=mean-sd, ymax=mean+sd,
-                fill=what),
+                fill=mtype(what)),
             data=segments, alpha=alpha.rect, color=NA)+
   geom_segment(aes(first-1/2, mean, xend=last+1/2, yend=mean,
-                   color=what),
+                   color=mtype(what)),
                data=segments)+
-  scale_fill_manual(values=model.colors)+
-  scale_color_manual(values=model.colors)
+  scale_y_continuous(breaks=seq(-4, 4, by=2))+
+  scale_fill_manual("distribution", values=model.colors, breaks=model.breaks)+
+  scale_color_manual("distribution", values=model.colors, breaks=model.breaks)
 w <- 7
 h <- 5
 pdf("figure-motivation-modelOnly.pdf",w=w,h=h)
@@ -261,17 +267,18 @@ breaksOnly <-
 ggplot()+
   geom_point(aes(base, signal), data=signals, pch=1)+
   geom_vline(aes(xintercept=change.base+1/2,
-               color=model.type),
+               color=mtype(model.type)),
            data=matching$changes, show_guide=TRUE, linetype="dashed")+
   scale_x_continuous("base position",
-                     breaks=segment.first)+
+                     breaks=base.breaks)+
   facet_grid(facet2 ~ facet)+
   theme_bw()+
   theme(panel.margin=grid::unit(0, "cm"))+
   geom_vline(aes(xintercept=change.after+1/2, color=what),
              data=changes, linetype="dashed")+
-  scale_fill_manual(values=model.colors)+
-  scale_color_manual(values=model.colors)
+  scale_y_continuous(breaks=seq(-4, 4, by=2))+
+  scale_fill_manual("breakpoints", values=model.colors, breaks=model.breaks)+
+  scale_color_manual("breakpoints", values=model.colors, breaks=model.breaks)
 pdf("figure-motivation-breaksOnly.pdf",w=w,h=h)
 print(breaksOnly)
 dev.off()
@@ -285,7 +292,7 @@ ggplot()+
   scale_color_manual(values=c(data="black", model="green"))+
   scale_fill_manual(values=c(data="black", model="green"))+
   scale_x_continuous("base position",
-                     breaks=segment.first)+
+                     breaks=base.breaks)+
   geom_rect(aes(xmin=first-1/2, xmax=last+1/2,
                 ymin=mean-sd, ymax=mean+sd,
                 fill=what),
@@ -312,7 +319,7 @@ ggplot()+
   geom_vline(aes(xintercept=change.after+1/2),
              data=changes, lty="dashed", color=model.colors[["truth"]])+
   scale_x_continuous("base position",
-                     breaks=segment.first)+
+                     breaks=base.breaks)+
   geom_rect(aes(xmin=first-1/2, xmax=last+1/2,
                 ymin=mean-sd, ymax=mean+sd),
             data=segments, alpha=2/10, color=NA, fill=model.colors[["truth"]])+
